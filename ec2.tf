81f9e691efe38a22618b656c632fdec7f5b8cbd7
Nieuport <millors21@gmail.com>
2023-08-21 18:30:18 +0000

    zmiany

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "latest_ubuntu" {
-  owners           = ["099720109477"]
+  owners      = ["099720109477"]
   most_recent = true
- 
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
@@ -9,14 +9,14 @@ data "aws_ami" "latest_ubuntu" {
 }
 
 resource "aws_instance" "panda" {
-  depends_on = [null_resource.download_ssh_key]
+  depends_on             = [null_resource.download_ssh_key]
   count                  = length(var.availability_zones)
   ami                    = data.aws_ami.latest_ubuntu.image_id
   instance_type          = "t2.micro"
   availability_zone      = var.availability_zones[count.index]
   key_name               = var.aws_key_name
   vpc_security_group_ids = [aws_security_group.sg_pub.id]
-  subnet_id = aws_default_subnet.default_az[count.index].id
+  subnet_id              = aws_default_subnet.default_az[count.index].id
 }
 
 resource "aws_security_group" "sg_pub" {
@@ -26,14 +26,14 @@ resource "aws_security_group" "sg_pub" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   egress {
     from_port   = 0
     to_port     = 65535
